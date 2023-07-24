a3285b50694a5f8ff26e501146aaff6a8a8efd6e
Minh Nguyen <122658896+minhnguyen220104@users.noreply.github.com>
2023-07-24 22:50:49 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from router import listsize
+
+app = FastAPI()
+
+list_router = [
+    listsize.router
+]
+for router in list_router:
+      app.include_router(router,prefix="/mobirun")
+
+origins = ["*"]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*'],
+)
+
+#models.Base.metadata.create_all(bind=engine)
